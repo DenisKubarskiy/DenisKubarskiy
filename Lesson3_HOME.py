012143f02e4a423dea65f2a5ba7c606c6f3be907
Denis Kubarskiy <kubarsky@mail.ru>
2020-04-03 05:51:14 +0300

    Домашнее задание 3

--- Lesson3_HOME.py
+++ Lesson3_HOME.py
@@ -8,7 +8,27 @@ f.close()
 
 # 1. Методами строк очистить текст от знаков препинания
 
-znaki=',?!—;:".«»()\r\t'
+znaki=',?!—;:".«»()\r\t\''
 for i in range(len(znaki)):
       text=text.replace(znaki[i],'')
+text=text.replace('\n',' ')
 print(text)
+
+# 2 Cформировать list со словами (split)
+
+wordlist=text.split(' ')
+print(wordlist)
+
+# 3 Привести все слова к нижнему регистру (map)
+
+L_wordlist = list(map(lambda x: x.lower(), wordlist))
+print(L_wordlist)
+
+# 4 Получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте
+
+Dic_text={}
+for i in range(len(L_wordlist)):
+      Dic_text=Dic_text.fromkeys=(L_wordlist[i],0)
+print(Dic_text.keys())
+
+
