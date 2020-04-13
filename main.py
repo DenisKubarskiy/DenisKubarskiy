53d2a67726e6c91ada8ee1aae4a97c85c6dc99e2
Denis Kubarskiy <kubarsky@mail.ru>
2020-04-14 01:29:41 +0300

    Домашнее задание 5

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a=1
-b=2
-print('1+2=',a+b)
-print('Hello, World!')
+import divisor_master
+m=int(input('Введите число '))
+
+divisor_master.simple(m)
+divisor_master.deliteli(m)
+divisor_master.biggest_simple_delitel(m)
+divisor_master.biggest_delitel(m)
+divisor_master.canonich(m)
